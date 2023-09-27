ecc65f47ee664943ab009d804d74a8704df68117
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-27 09:40:12 -0400

    Part 4 of Lab 5 Unit 5

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,23 @@
-public class Main {
+import java.util.Scanner;
+
+public class TheaterKiosk {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int userAge = 0;
+
+
+        System.out.println("Please enter your age, if you're aged 21 or older you will be issued a wristband.");
+        userAge = scan.nextInt();
+
+        if (userAge >= 21){
+            System.out.println("Congratulations you are old enough to receive a wristband.");
+        }
+        else if (userAge > 0){
+            System.out.println("Sorry you are not old enough to receive a wristband.");
+        }
+        else {
+            System.out.println("Nice try pal. Try again.");
+        }
     }
 }
